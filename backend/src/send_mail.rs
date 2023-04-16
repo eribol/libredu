1490ebc9d9919bde172a760762e7c5ab5ff2ea6a
Mehmet Eribol <mehmeteribol@gmail.com>
2023-04-16 19:08:26 +0300

    sendmail changes

--- backend/src/send_mail.rs
+++ backend/src/send_mail.rs
@@ -1,6 +1,5 @@
 use lettre::message::header::ContentType;
-use lettre::transport::smtp::authentication::Credentials;
-use lettre::{Message, SmtpTransport, Transport};
+use lettre::{Message, SendmailTransport, Transport};
 
 pub fn send_mail(sender: String, email: String, password: String, body: String){
 let email = Message::builder()
@@ -12,21 +11,6 @@ let email = Message::builder()
     .header(ContentType::TEXT_HTML)
     .body(body)
     .unwrap();
-    let tls = lettre::transport::smtp::client::TlsParameters::builder("smtp-relay.gmail.com".to_owned())
-    .dangerous_accept_invalid_certs(true)
-    .build().unwrap();
-    let creds = Credentials::new(sender.to_owned(), password.to_owned());
-
-    // Open a remote connection to gmail
-    let mailer = SmtpTransport::relay("smtp-relay.gmail.com")
-        .unwrap()
-        .credentials(creds)
-        .tls(lettre::transport::smtp::client::Tls::Required(tls))
-        .build();
-
-    // Send the email
-    match mailer.send(&email) {
-        Ok(_) => println!("Email sent successfully!"),
-        Err(e) => panic!("Could not send email: {e:?}"),
-    }
+    let sender = SendmailTransport::new();
+    let result = sender.send(&email);
 }
