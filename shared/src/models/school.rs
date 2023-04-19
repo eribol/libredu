74add37c4f7af98293d5417bcb3e5c5442c391d2
Mehmet Eribol <mehmeteribol@gmail.com>
2023-04-19 23:16:24 +0300

    Added phone future for validate update school

--- shared/src/models/school.rs
+++ shared/src/models/school.rs
@@ -1,10 +1,12 @@
 use moonlight::*;
+use validator::Validate;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize, Validate, Default)]
 #[serde(crate = "serde")]
 pub struct FullSchool {
-    //pub id: i32,
+    #[validate(length(min = 2, max = 100))]
     pub name: String,
     pub manager: i32,
+    #[validate(phone)]
     pub phone: String,
 }
