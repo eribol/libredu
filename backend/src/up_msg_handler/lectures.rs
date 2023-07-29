3438ae83f528b52dc3fc672431744a9f3672dc26
Mehmet Eribol <mehmeteribol@gmail.com>
2023-07-29 11:05:30 +0300

    Removed postgres conn RwLock write(it locks connections)

--- backend/src/up_msg_handler/lectures.rs
+++ backend/src/up_msg_handler/lectures.rs
@@ -8,7 +8,7 @@ use super::auth::POSTGRES;
 use moon::tokio_stream::StreamExt;
 
 pub async fn add_lecture(id: i32, lecture_form: AddLecture) -> DownMsg {
-    let db = POSTGRES.write().await;
+    let db = POSTGRES.read().await;
     let mut row = sqlx::query(
         r#"insert into subjects(kademe, name, school, short_name) 
         values($1, $2, $3, $4) returning id, kademe, name, short_name"#,
@@ -35,7 +35,7 @@ pub async fn add_lecture(id: i32, lecture_form: AddLecture) -> DownMsg {
 }
 
 pub async fn update_lecture(school_id: i32,lecture_form: Lecture) -> DownMsg {
-    let db = POSTGRES.write().await;
+    let db = POSTGRES.read().await;
     let _row = sqlx::query(
         r#"update subjects set kademe = $1, name = $2, short_name = $3
         where id = $4 and school = $5
@@ -52,7 +52,7 @@ pub async fn update_lecture(school_id: i32,lecture_form: Lecture) -> DownMsg {
 }
 
 pub async fn get_lectures(id: i32) -> DownMsg {
-    let db = POSTGRES.write().await;
+    let db = POSTGRES.read().await;
     let mut lectures: Vec<Lecture> = vec![];
     let mut row = sqlx::query(
         r#"select id, kademe, name, short_name from subjects
@@ -72,7 +72,7 @@ pub async fn get_lectures(id: i32) -> DownMsg {
 }
 
 pub async fn del_lecture(id: i32, school_id: i32) -> DownMsg {
-    let db = POSTGRES.write().await;
+    let db = POSTGRES.read().await;
     let mut row = sqlx::query(
         r#"delete from subjects where id = $1 and school = $2 returning id"#,
     )
