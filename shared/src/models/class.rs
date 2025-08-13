377f46ab9f1695d2aa15fc79a394d5c29a91b3f5
Mehmet ERİBOL <mehmeteribol@gmail.com>
2025-08-13 05:50:42 +0300

    Api hazırlandı

--- shared/src/models/class.rs
+++ shared/src/models/class.rs
@@ -5,17 +5,17 @@ pub type UserId = EntityId;
 #[derive(Debug, Serialize, Deserialize, Clone, Validate)]
 #[serde(crate = "serde")]
 pub struct AddClass {
-    #[validate(length(min=1, max = 5))]
-    pub kademe: String,
-    #[validate(length(min=1, max = 5))]
-    pub sube: String,
+    #[validate(length(min = 1, max = 5))]
+    pub grade: String,
+    #[validate(length(min = 1, max = 5))]
+    pub branch: String,
     pub group_id: i32,
 }
-impl AddClass{
-    pub fn is_valid(&self)-> Result<(), ValidationErrors>{
+impl AddClass {
+    pub fn is_valid(&self) -> Result<(), ValidationErrors> {
         self.validate()
     }
-    pub fn has_error(&self, field: &'static str)->bool{
+    pub fn has_error(&self, field: &'static str) -> bool {
         ValidationErrors::has_error(&self.is_valid(), field)
     }
 }
@@ -24,22 +24,25 @@ impl AddClass{
 #[serde(crate = "serde")]
 pub struct Class {
     pub id: i32,
-    pub kademe: String,
-    pub sube: String,
+    pub grade: String,
+    pub branch: String,
     pub group_id: i32,
+    pub short_name: String,
+    pub name: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(crate = "serde")]
 pub struct ClassLimitation {
     pub class_id: i32,
-    pub day: i32,
-    pub hours: Vec<bool>,
+    // pub day: i32,
+    // pub hours: Vec<bool>,
+    pub limitations: Vec<Vec<bool>>,
 }
 
-impl Class{
-    pub fn label(&self)->String{
-        let label = self.kademe.clone() + &self.sube;
+impl Class {
+    pub fn label(&self) -> String {
+        let label = self.grade.clone() + &self.branch;
         label
     }
 }
--- shared/src/models/timetables.rs
+++ shared/src/models/timetables.rs
@@ -15,20 +15,21 @@ pub struct Timetable {
 }
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(crate = "serde")]
-pub struct TimetableSchedules{
+pub struct TimetableSchedules {
     pub group_id: i32,
     pub starts: Vec<NaiveTime>,
-    pub ends: Vec<NaiveTime>
+    pub ends: Vec<NaiveTime>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(crate = "serde")]
 pub struct Activity {
     pub id: i32,
-    pub subject: i32,
+    pub lecture: i32,
     pub hour: i16,
     pub classes: Vec<i32>,
     pub teachers: Vec<i32>,
-    pub blocks: Option<String>,
-    pub partner_activity: Option<i32>,
+    pub no_limit: bool,
+    pub partners: Vec<i32>,
+    pub classrooms: Vec<i32>,
 }
